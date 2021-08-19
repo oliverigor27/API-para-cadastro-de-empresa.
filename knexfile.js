8bfcbbb62ee31c1c641cb6197e446eadbb0e828b
Igor Oliveira <igoroliveira2798@gmail.com>
2021-08-19 00:47:18 -0300

    Create knexfile

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+require("dotenv").config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    },
+    migrations: {
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  }
+
+};
